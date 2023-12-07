65542dd8b2557b77414df9323cc7386b8a7057fc
owahlgren <owahlgren@polygon.technology>
2023-12-07 12:32:26 -0500

    feat: multi-channel support

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import { TwitchAuthResponse, TwitchStreamResponse } from './interfaces'
 import { streamers } from './streamers';
 import { TextChannel } from 'discord.js';
 
-const { discord_token, twitch_client_id, twitch_secret, discord_channel_id } = require('./config.json');
+const { discord_token, twitch_client_id, twitch_secret, discord_channel_ids } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+client.login(discord_token)
 
 const getTwitchOath = async (): Promise<string> => {
     const res = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${twitch_client_id}&client_secret=${twitch_secret}&grant_type=client_credentials`, {
@@ -37,18 +39,27 @@ interface History {
 const history: History = {}
 
 const checkStreamers = async () => {
+    console.log('polling streamers...')
     for (const streamer of streamers) {
         const streamInfo = await getStreamInfo(streamer)
         if (isStreamLive(streamInfo)) {
             if (history[streamer] !== true) {
-                const channel = client.channels.cache.get(discord_channel_id) as TextChannel
-                channel.send(`@here ${streamer} is live!\n${streamInfo.data[0].title}\nhttps://twitch.tv/${streamer}\n`)
-                console.log(`channel alerted for ${streamer}`)
+                alertChannels(streamer, streamInfo)
             }
             history[streamer] = true
         } else {
             history[streamer] = false
         }
     }
-} 
-setInterval(checkStreamers, 60000) // 60000 milliseconds = 1 minute
+}
+
+const alertChannels = async (streamer: string, streamInfo: TwitchStreamResponse) => {
+    const channelIds: Array<string> = discord_channel_ids
+    for (const channelId of channelIds) {
+        const channel = client.channels.cache.get(channelId) as TextChannel
+        channel.send(`@here ${streamer} is live!\n${streamInfo.data[0].title}\nhttps://twitch.tv/${streamer}\n`)
+        console.log(`channel ${channelId} alerted for ${streamer}`)
+    }
+    return
+}
+setInterval(checkStreamers, 15000) // 60000 milliseconds = 1 minute
